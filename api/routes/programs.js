5e1226d6cc9884ebfbab62afa078e12a8534343b
Eric Lindgren <elindgren05@gmail.com>
2016-10-22 16:29:37 -0500

    Relational api calls added

--- api/routes/programs.js
+++ api/routes/programs.js
@@ -10,4 +10,32 @@ router.route('/')
   });
 })
 ;
+router.route('/services')
+.get(function(req, res) {
+  var programId = req.body.programId;
+  global.db.ref('programs/' + programId + '/services/').once('value').then(function(snapshot) {
+    res.json(snapshot.val());
+  });
+})
+.post(function(req, res) {
+  var programId = req.body.programId;
+  var serviceId = req.body.serviceId;
+  var programData;
+  var serviceData;
+  global.db.ref('services/' + serviceId).once('value').then(function(snapshot) {
+    serviceData = snapshot.val();
+    serviceData.programs = serviceData.programs || {};
+    serviceData.programs[programId] = true;
+  }).then(function() {
+    global.db.ref('services/' + serviceId).set(serviceData);
+  });
+  global.db.ref('programs/' + programId).once('value').then(function(snapshot) {
+    programData = snapshot.val();
+    programData.services = programData.services || {};
+    programData.services[serviceId] = true;
+  }).then(function() {
+    global.db.ref('programs/' + programId).set(programData);
+  });
+})
+;
 module.exports = router;
