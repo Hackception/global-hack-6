3591caa60ba4ddf96a27e3ad9a521e129d96ac33
Eric Lindgren <elindgren05@gmail.com>
2016-10-22 17:13:58 -0500

    Added key based root level updates

--- api/routes/locations.js
+++ api/routes/locations.js
@@ -1,11 +1,12 @@
 var router = require('express').Router();
 router.route('/')
 .post(function(req, res) {
-  var newPostKey = global.db.ref().child('locations').push().key;
+  var newPostKey = req.body.key || global.db.ref().child('locations').push().key;
   res.json(global.db.ref('locations/' + newPostKey).set(req.body).key);
 })
 .get(function(req, res) {
-  global.db.ref('locations/').once('value').then(function(snapshot) {
+  var search = 'locations/' + (req.body.key || '');
+  global.db.ref(search).once('value').then(function(snapshot) {
     res.json(snapshot.val());
   });
 })
