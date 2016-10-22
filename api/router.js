2783655e57ea2887b57f359d7b68ee10908f051b
Alex Mudd <Alex Mudd>
2016-10-22 16:03:03 -0500

    clean up

--- api/router.js
+++ api/router.js
@@ -6,5 +6,4 @@ router.use('/locations', require('./routes/locations'));
 router.use('/programs', require('./routes/programs'));
 router.use('/services', require('./routes/services'));
 
-
 module.exports = router;
