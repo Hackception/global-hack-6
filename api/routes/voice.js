08b5f89a0578ed82a98802c3befa35326ba96f0c
Alex Mudd <Alex Mudd>
2016-10-23 00:19:20 -0500

    moved web endpoint into the service file, cleaned up POST for /api/questions/id so it doesn't catch every POST request to /api/questions

--- api/routes/voice.js
+++ api/routes/voice.js
@@ -1,12 +1,8 @@
 var router = require('express').Router(),
-questionPath = require('../constants/question-path'),
 voice = require('../twilio/voice');
 
-router.post('/:index?', voice.question);
-router.route('/web')
-.get(function(req, res) {
-  res.json(questionPath);
-});
-
+router.post('/:index(\\d+)?', voice.question);
+router.post('/finish', voice.finishUp);
+router.get('/web', voice.listQuestions);
 
 module.exports = router;
